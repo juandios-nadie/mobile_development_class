97b8676139e2a2eab6d4b246e474be1c5d41a3cd
Juan de Dios De la Torre González <141748092+juandios-nadie@users.noreply.github.com>
2025-09-29 10:15:02 -0600

    swift app actually

---
+++ app_1/app_1Tests/app_1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  app_1Tests.swift
+//  app_1Tests
+//
+//  Created by Juan de Dios De la Torre Gonzalez on 29/09/25.
+//
+
+import Testing
+@testable import app_1
+
+struct app_1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
